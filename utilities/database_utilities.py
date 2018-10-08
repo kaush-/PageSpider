869bc9d6a66a8cd5281e19b1772c5ea76852b871
pikaushi <pikaushi@akamai.com>
2018-10-08 12:20:29 +0200

    Final commit.

---
+++ utilities/database_utilities.py
@@ -0,0 +1,31 @@
+import sqlite3 as lite
+
+
+def create_database(database_path: str):
+    conn = lite.connect(database_path)
+    with conn:
+        cur = conn.cursor()
+        cur.execute("Drop table if EXISTS words")
+        ddl = "create table words ( word text not null primary key, usage_count int default 1 not null);"
+        cur.execute(ddl)
+        ddl = "create unique index words_words_uindex on words (word);"
+        cur.execute(ddl)
+    conn.close()
+
+
+def save_words_to_database(database_path: str, words_list: list):
+    conn = lite.connect(database_path)
+    with conn:
+        cur = conn.cursor()
+        for word in words_list:
+            # check to see if the word is in there
+            sql = "select count(word) from words where word='" + word + "'"
+            cur.execute(sql)
+            count = cur.fetchone()[0]
+            if count > 0:
+                sql = "update words set usage_count = usage_count + 1 where word = '" + word + "'"
+            else:
+                sql = "insert into words(word) values ('" + word + "')"
+            cur.execute(sql)
+
+        print("Database save complete!")
